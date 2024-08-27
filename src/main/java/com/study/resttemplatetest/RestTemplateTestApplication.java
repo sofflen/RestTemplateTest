e134acad3e45e149974ce022aaed183d488ace7b
sofflen <nixxesgaming@gmail.com>
2024-08-27 18:25:50 +0300

    initial commit

---
+++ src/main/java/com/study/resttemplatetest/RestTemplateTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.resttemplatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/resttemplatetest/RestTemplateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.resttemplatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
